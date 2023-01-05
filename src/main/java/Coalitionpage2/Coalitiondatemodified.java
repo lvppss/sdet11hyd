4cb3c2092164d9df581ce9db7818d699ce45c1e2
Personal <Personal@DESKTOP-L12EV7F>
2023-01-05 07:41:32 +0530

    Classes under subbranch

---
+++ src/main/java/Coalitionpage2/Coalitiondatemodified.java
@@ -0,0 +1,9 @@
+package Coalitionpage2;
+
+public class Coalitiondatemodified {
+	public static void main(String[]args)
+	{
+		System.out.println("coalition date modified");
+	}
+
+}
---
+++ src/main/java/Coalitionpage2/Conductcoalition.java
@@ -0,0 +1,9 @@
+package Coalitionpage2;
+
+public class Conductcoalition {
+	public static void main(String[]args)
+	{
+		System.out.println("coalitionconducted");
+	}
+
+}
