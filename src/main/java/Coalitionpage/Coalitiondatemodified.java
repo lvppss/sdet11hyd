5d65e55548e334e45a23afa0393eeb0b1c1e5a25
Personal <Personal@DESKTOP-L12EV7F>
2023-01-05 07:38:42 +0530

    classes under master branch

--- src/main/java/Coalitionpage/Coalitiondatemodified.java
+++ src/main/java/Coalitionpage/Coalitiondatemodified.java
@@ -1,5 +1,9 @@
 package Coalitionpage;
 
 public class Coalitiondatemodified {
+	public static void main(String[]args)
+	{
+		System.out.println("coalition date modified");
+	}
 
 }
--- src/main/java/Coalitionpage/Conductcoalition.java
+++ src/main/java/Coalitionpage/Conductcoalition.java
@@ -1,5 +1,9 @@
 package Coalitionpage;
 
 public class Conductcoalition {
+	public static void main(String[]args)
+	{
+		System.out.println("coalitionconducted");
+	}
 
 }
